975ee9143c9533a6ff95432a6dd97152936b04e4
Naomi Liu <nhliu@ucdavis.edu>
2021-01-06 20:43:57 +0000

    Added page to display new image

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
@@ -6,6 +6,11 @@ app = Flask(__name__)
 def upload():
     "show page for user to upload picture"
     return render_template("color-changer.html")
+
+@app.route("/greyed")
+def display():
+    "displays grey scale version of the picture"
+    return render_template("changed.html")
 
 if __name__ == "__main__":    
     app.run(debug=True, host='0.0.0.0')
--- static/color-changer.js
+++ static/color-changer.js
@@ -1,34 +1,39 @@
 "use strict" 
-// Use js vanilla this time
-// confirm link between 
-console.log("hi");
-//submit not a function -  go back and look at fat arrow function used once
-document.querySelector('#btn').submit(() =>{
-    let pic = document.querySelector('#picture').open();
-    newPic = grey(pic);
-    document.querySelector('#new-pic') = newPic;
-})
-document.querySelector('#btn').submit
 
-function grey(pic){
-    const canvas = document.createElement('canvas'); //creates html tag canvas which is required to draw graphics
-    const canvasContext = canvas.getContext('2d'); //allows for drawing on the canvas
-    //part of the canvas api and gives 2D rendering context to draw on a canvas element
+// 1. get picture uploaded and displayed back to the user
+$('#btn').submit(() => {
+    const formVal = $("#picture").serialize();
+    console.log(formVal);
+    // let pic = $('#picture').open();
+    // newPic = grey(pic);
+    // document.querySelector('#new-pic') = newPic;
+});
+
+
+// function grey(pic){
+//     const canvas = document.createElement('canvas'); //creates html tag canvas which is required to draw graphics
+//     const canvasContext = canvas.getContext('2d'); //allows for drawing on the canvas
+//     //part of the canvas api and gives 2D rendering context to draw on a canvas element
     
-    canvas.width = pic.width; //assign width of canvas element to be the same width as the picture
-    canvas.height = pic.height;
+//     canvas.width = pic.width; //assign width of canvas element to be the same width as the picture
+//     canvas.height = pic.height;
 
-    canvasContext.drawImage(pic, 0, 0); //drawImage provides different ways to draw image on canvas
-    //dx = 0 and dy = 0 specifies position (0,0) = (x,y) where the top left point of the picture should
-    //be on the canvas. Here we have them line up.
-    const imgPixels = canvasContext.getImageData(0, 0, pic.width, pic.height); //provides pixel data
-    //for a portion of the canvas from the top left corner (0,0). With a width of pic.width and height of pic.height
-    for(let y = 0; y < imgPixels.height; y++){ //iterate through y values after changing all the x values pixels for that y. starts filling from bottom left
-        for(let x = 0; x < imgPixels.width; x++){
-
+//     canvasContext.drawImage(pic, 0, 0); //drawImage provides different ways to draw image on canvas
+//     //dx = 0 and dy = 0 specifies position (0,0) = (x,y) where the top left point of the picture should
+//     //be on the canvas. Here we have them line up.
+//     const imgPixels = canvasContext.getImageData(0, 0, pic.width, pic.height); //provides pixel data
+//     //for a portion of the canvas from the top left corner (0,0). With a width of pic.width and height of pic.height
+//     for(let y = 0; y < imgPixels.height; y++){ //iterate through y values after changing all the x values pixels for that y. starts filling from bottom left
+//         for(let x = 0; x < imgPixels.width; x++){
+//             var i = (y * 4) * imgPixels.width + x * 4; //formula to find the 1D location; i represents each dot location?
+//             //usually pixelLocation = y * imgPixel.width + x
+//             var avg = (imgPixels.data[i] + imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3;
+//             imgPixels.data[i] = avg; 
+//             imgPixels.data[i + 1] = avg; 
+//             imgPixels.data[i + 2] = avg;
             
-        }
-    }
-    return canvasContext.putImageData(imgPixels, 0, 0, 0, 0, imgPixels.width, imgPixels.height);
+//         }
+//     }
+//     return canvasContext.putImageData(imgPixels, 0, 0, 0, 0, imgPixels.width, imgPixels.height);
     
-}
+// }
